6cdb582cf27bd06f157af72a0eadec55ce9ea7a5
Jonathan Robbins <jonathan.robbins@rcn.org.uk>
2020-06-20 09:47:09 +0100

    restructured folder for service worker

--- dist/app.js
+++ dist/app.js
@@ -1,9 +1,9 @@
+"use strict";
 if ('serviceWorker' in navigator) {
     navigator.serviceWorker
-    .register('/dist/sw.js')
-    .then(function() {
+        .register('/dist/sw.js')
+        .then(function () {
         console.log('Service worker registered!');
     });
 }
-
 let hello = 'hii';
